45c1a4249356d47779c526414660b750dd15535a
Andreacarolina47 <93480002+Andreacarolina47@users.noreply.github.com>
2022-05-18 11:20:05 +0800

    Created project file, installed bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+      <a href='/' className='btn btn-primary'>Search</a>
+    </div>
+  );
+}
+
+
