b8cc8e7d3d4d73b4ca43ebd2efad68c1235ff4e3
Andreacarolina47 <93480002+Andreacarolina47@users.noreply.github.com>
2022-05-22 22:29:40 +0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
       <Dictionary/>
+      <Footer />
     </div>
     
   );
---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+      This project was coded by:{" "}
+      <a href= "https://happy-sammet-7f040f.netlify.app/" target="_blank"
+        rel="noreferrer"
+        >Andrea Gómez</a>{" "}
+        Open-sourced on{" "}
+      <a
+        href="https://github.com/Andreacarolina47/Dictionary-project"
+        target="_blank"
+        rel="noreferrer"
+        
+      >
+        GitHub </a
+      > 
+    </footer>
+    )
+}
