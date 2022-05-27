9b388fbebc51480b50a6bbad13d8a5069dd60315
Andreacarolina47 <93480002+Andreacarolina47@users.noreply.github.com>
2022-05-27 12:34:22 +0800

    Added fixed word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,16 +3,21 @@ import axios from "axios";
 import Results from "./Results";
 
 export default function Dictionary(){
-    const [word, setWord]= useState(null);
+    const [word, setWord]= useState("Dictionary");
     const[results, setResults]= useState(null);
+    const [loaded, setLoaded]= useState(false);
 
     function handleDefinition(response){
         setResults(response.data[0]);
     }
-
-    function search(event){
+     
+    function handleSubmit(event){
+        setLoaded(true);
         event.preventDefault();
-        
+        search();
+    }
+
+    function search(){
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         axios.get(apiUrl).then(handleDefinition);
     }
@@ -21,13 +26,17 @@ export default function Dictionary(){
         event.preventDefault();
         setWord(event.target.value);
     }
-
+    if (loaded){
     return(
     <div>
-    <form onSubmit={search}>
+    <form onSubmit={handleSubmit}>
         <input type="search" placeholder="Type a word" autoFocus="on" onChange={handleWordChange}/>
         <input type="submit" value="Search🔍" />
     </form>
     <Results results={results}/>
     </div>)
-}
+}else{
+    setLoaded(true);
+    search();
+    return null;
+}}
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Phonetics(props){
+    if(props.phonetics.audio){
     return(
     <div>
         <a href={props.phonetics.audio} target="_blank" rel="noreferrer" >🔊</a>
         {props.phonetics.text}
     </div>);
-}
+    }else {
+        return null;
+    }}
